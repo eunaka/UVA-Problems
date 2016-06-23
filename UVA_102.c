2abe716b7c69576bac12f4a4c6387741623d7cfe
yuri971 <yurinakamora97@gmail.com>
2016-06-22 22:43:25 -0300

    Update UVA_102.c

--- UVA_102.c
+++ UVA_102.c
@@ -1,8 +1,36 @@
-#include <stdio.h>
+#include <iostream>
 #include <stdlib.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> entrada(string& input)
+{
+    string aux;                     //string auxiliar, recebe cada número da string original sem os espaços
+    int num , i=-1, pos;
+    vector<int> v;                  //vetor que vai armazenar os numeros
+    while(true)
+    {
+        if(input.at(++i) != ' ')
+        {
+            pos = input.find(" ", i);       //find retorna a primeira aparição(a partir de 'i') do espaço
+            aux.append(input, i, pos-i);    //anexa/adiciona em aux a substring de input a partir de i com amanho pos-i(length da substring)
+            num = stoi(aux);                //converte os numeros da string em inteiros
+            v.push_back(num);               //adiciona o numero ao vetor
+            aux.clear();                    //limpa a string auxiliar para poder receber novas substrings
+            i = pos;                        //i = posição que estava o espaço em branco
+            if(i > input.size())
+                break;
+        }
+    }
+    return v;
+}
 
 int main()
 {
+    string s;
+    cin >> s;
 	int garrafas[] = {1,2,3,4,5,6,7,8,9};                          // Este vetor que vai receber os valores inteiros da string, implementar essa conversao
 
 	int e[3];                                                      // vetor com a caixa da esquerda
@@ -62,22 +90,22 @@ int main()
 	switch(j)
 	{
 		case 0:
-			printf("BGC %d", menor);
+		    cout << "BGC " << menor;
 			break;
 		case 1:
-			printf("BCG %d", menor);
+			cout << "BCG " << menor;
 			break;
 		case 2:
-			printf("GBC %d", menor);
+			cout << "GBC " << menor;
 			break;
 		case 3:
-			printf("GCB %d", menor);
-			break;
+			cout << "GCB " << menor;
+			break
 		case 4:
-			printf("CBG %d", menor);
+			cout << "CBG " << menor;
 			break;
 		default:
-			printf("CGB %d",j, menor);
+			cout << "CGB " << menor;
 	}
 
 	return 16;  // so pra testar, tava dando merda
